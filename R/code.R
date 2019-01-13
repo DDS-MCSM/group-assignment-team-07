52a009c64a4b3332612fdc48bf2a0c9acc590d4e
Ian <iansangines@gmail.com>
2019-01-13 14:08:01 +0100

    Finished data accquisition and started piechart

--- R/code.R
+++ R/code.R
@@ -1,4 +1,4 @@
-#' Title
+ #' Title
 #'
 #' @details This are the details
 #' @return
@@ -13,9 +13,11 @@ library("httr")
 library("reticulate")
 library("curl")
 library("magrittr")
+library("tidyr")
+library("dplyr")
 
-existing_domains <<- data.frame(Testing = character(), Has_certificate = logical() , Organization = character())
-non_existing_domains <<- list()
+domains_df <<- data.frame(Domain = character(), Has_certificate = logical() , Organization = character(), Is_reliable = logical(), Resolves = logical())
+non_domains_df <<- list()
 reliable_organization <<- ""
 
 domain_exists <- function(domain_to_check){
@@ -31,6 +33,9 @@ domain_exists <- function(domain_to_check){
 }
 
 main <- function(domain){
+  domains_df <<- data.frame(Domain = character(), Has_certificate = logical() , Organization = character(), Is_reliable = logical(), Resolves = logical())
+  non_domains_df <<- list()
+  reliable_organization <<- ""
 
   #Get reliable domain data (Organiation)
   if (!domain_exists(domain)) {
@@ -39,7 +44,7 @@ main <- function(domain){
   # Get Organization from the certificate
   sslshopper_url <- paste("https://www.sslshopper.com/assets/snippets/sslshopper/ajax/ajax_check_ssl.php?hostname=", domain, "&g-recaptcha-response=&rand=190", sep = "")
   sslshopper_htmlpage <- GET(sslshopper_url, add_headers("X-Requested-With" = "XMLHttpRequest"))
-  df <- get_organization_certificate_info(sslshopper_htmlpage)
+  df <- get_organization_certificate_info(sslshopper_htmlpage, known_as_reliable = TRUE)
   reliable_organization <<- df$Organization
 
   #get similar domains list
@@ -49,8 +54,8 @@ main <- function(domain){
   pool <- new_pool()
   cb <- function(req){
     print("Entered done function")
-    existing_domains <<- rbind(existing_domains, get_organization_certificate_info(req$content))
-    print(existing_domains)
+    domains_df <<- rbind(domains_df, get_organization_certificate_info(req$content,known_as_reliable = FALSE))
+    print(domains_df)
   }
 
   # Getting data from every similar domain
@@ -58,7 +63,8 @@ main <- function(domain){
     if (domain_exists(similar_domains[i])) {
       curl_fetch_multi(paste("https://www.sslshopper.com/assets/snippets/sslshopper/ajax/ajax_check_ssl.php?hostname=", similar_domains[i], "&g-recaptcha-response=&rand=190", sep = ""),pool = pool, handle = new_handle() %>% handle_setheaders("X-Requested-With" = "XMLHttpRequest"), done = cb)
     } else {
-      non_existing_domains <<- c(non_existing_domains, similar_domains[i])
+      non_domains_df <<- c(non_domains_df, similar_domains[i])
+      domains_df <<- data.frame(Domain = similar_domains[i], Has_certificate = FALSE , Organization = "None", Is_reliable = FALSE, Resolves = FALSE)
     }
   }
    out <- multi_run(pool = pool)
@@ -68,8 +74,8 @@ main <- function(domain){
 
 ## FUNCTION get_sslshopper certficate info -----------------------------------------------------
 
-get_organization_certificate_info <- function(response){
-  organization <- "None"
+get_organization_certificate_info <- function(response, known_as_reliable){
+  organization <- character(0)
   sslshopper_html <- read_html(response)
 
   resolve_text <- xml_text(xml_find_all(sslshopper_html, "//h3[contains(.,'resolves')]"))
@@ -85,25 +91,47 @@ get_organization_certificate_info <- function(response){
       if (has_organization_field) {
         organization <- xml_text(xml_find_all(sslshopper_html,"//table[2]/tr[1]/td[2]/descendant::text()[6]"))
       }
-      else{
-        #don't have organization field -> get organization from https://www.ultratools.com/tools/ipWhoisLookupResult with ip
-        resolve_text <- xml_text(xml_find_all(sslshopper_html, "//h3[contains(.,'resolves')]"))
-        domain_ip <- regmatches(resolve_text, gregexpr("\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b",resolve_text))[1]
-        ultratools_url <- "https://www.ultratools.com/tools/ipWhoisLookupResult"
-        ultratools_htmlpage <- httr::POST(ultratools_url,add_headers("Content-Type" = "application/x-www-form-urlencoded"), body = paste("ipAddress=",domain_ip, sep = ""))
-        ultratools_html <- read_html(ultratools_htmlpage)
-        organization <- xml_text(xml_find_all(ultratools_html,"//div[contains(./span,'Org:')]/span[2]"))
-        if (identical(organization,character(0))) {
-          organization <- "None"
-        }
-      }
-    } else {
-      #No certificate
     }
-    result <- data.frame(Testing = html_domain, Has_certificate = has_certificate, Organization = organization)
+    if (identical(organization,character(0))){
 
-    print(result)
+      #don't have organization field -> get organization from https://www.ultratools.com/tools/ipWhoisLookupResult with ip
+      resolve_text <- xml_text(xml_find_all(sslshopper_html, "//h3[contains(.,'resolves')]"))
+      domain_ip <- regmatches(resolve_text, gregexpr("\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b",resolve_text))[1]
+      ultratools_url <- "https://www.ultratools.com/tools/ipWhoisLookupResult"
+      ultratools_htmlpage <- httr::POST(ultratools_url,add_headers("Content-Type" = "application/x-www-form-urlencoded"), body = paste("ipAddress=",domain_ip, sep = ""))
+      ultratools_html <- read_html(ultratools_htmlpage)
+      organization <- xml_text(xml_find_all(ultratools_html,"//div[contains(./span,'Org:')]/span[2]"))
+      if (identical(organization,character(0))) {
+        organization <- "None"
+      }
+
+    }
+
+    result <- data.frame(Domain = html_domain, Has_certificate = has_certificate, Organization = organization, Is_reliable = ((gsub("\\s","",tolower(organization)) == gsub("\\s","",tolower(reliable_organization))) | known_as_reliable), Resolves = TRUE)
   return(result)
 }
 
+draw_pie <- function(df) {
 
+  # Reliable domains, with a correct certificate and Organization == Reliable Organization
+  reliability_domains <- (df %>% filter(Is_reliable == TRUE) %>% count())[[1]]
+  print(reliability_domains)
+  # Not Reliable domains with certificate of organization != Reliable organization
+  not_reliable_certificate_domains <- (df %>% filter(Has_certificate == TRUE & Is_reliable == FALSE) %>% count())[[1]]
+  print(not_reliable_certificate)
+  # Not Reliable domains wothout certificate that resolves
+
+  not_reliable_without_certificate_domains <- (df %>% filter(Has_certificate == FALSE & Is_reliable == FALSE & Resolves == TRUE) %>% count())[[1]]
+  print(not_reliable_without_certificate)
+  # Not resolving domains
+  not_resolving_domains <- as.data.frame(df %>% filter(Resolves == FALSE) %>% count())
+  print(resolves_df)
+  pie(x=c(14,22,15,3,15,33,0,6,45),labels="",
+      col=c("#f21c39","#dba814","#7309de"))
+  par(new=TRUE)
+  pie(x=c(1),labels=c("Peligro"),radius=0.6,
+      col=c("#f21c39"))
+  par(new=TRUE)
+  pie(x=c(10,0),labels=c(".","Todo bien"),radius=0.3,
+      col=c("#7309de","#7309de"))
+}
--- python_scripts/edit_domain.py
+++ python_scripts/edit_domain.py
@@ -3,7 +3,8 @@ def edit_domain(domain) :
   splitted_domain = domain.split('.')
   word = splitted_domain[0]
 
-  letters = 'a4iIl0o1uvbdpqs5|'
+  #letters = 'a4iIl0o1uvbdpqs5|'
+  letters = ''
   splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
   deletes = [L + R[1:] for L, R in splits if R]
   transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
